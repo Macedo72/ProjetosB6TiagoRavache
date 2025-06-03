2fdf6aa3b14e0daf5e7aaf0e775eba1b9d255d68
Macedo72 <joaogabrielmacedops@gmail.com>
2025-06-03 20:30:11 -0300

    add caracteristicas da avl

--- src/arvoresAVL/ArvoreAVL.java
+++ src/arvoresAVL/ArvoreAVL.java
@@ -1,5 +1,17 @@
 package arvoresAVL;
 
 public class ArvoreAVL {
+    private Node raiz;
 
+    private int altura(Node node){
+        return (node == null)? 0 : node.altura;
+    }
+
+    private int getFatorBalan(Node node){
+        return (node == null) ? 0 : altura(node.esquerda) - altura(node.direita);
+    }
+    private void atualizarAlt (Node node){
+        node.altura = 1 + Math.max(altura(node.esquerda), altura(node.direita));
+    }
+    
 }
